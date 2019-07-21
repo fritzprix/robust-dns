34301a5b800ffc396374ac7ddaaf8feaed6d6a23
fritzprix <innocentevil0914@gmail.com>
2019-07-21 18:56:23 +0900

    optimize resource usage - cancel ongoing requests just after reception of the response first arrived - handles all the exception thrown during DNS resolution by returning DnsRecord object

--- src/main/java/com/doodream/robustdns/RobustDnsResolver.java
+++ src/main/java/com/doodream/robustdns/RobustDnsResolver.java
@@ -3,7 +3,10 @@ package com.doodream.robustdns;
 import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xbill.DNS.*;
 
 import java.net.Inet4Address;
@@ -13,24 +16,15 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 public class RobustDnsResolver {
 
+    private static final Logger Log = LoggerFactory.getLogger(RobustDnsResolver.class);
     private boolean updateOnExpire;
     private long cacheTimeoutInMills;
 
-    public static class DnsRecord {
-        InetAddress address;
-        long expireAt;
-
-        public DnsRecord(InetAddress resolved, long expireAt) {
-            address = resolved;
-            this.expireAt = expireAt;
-        }
-    }
 
     private final ConcurrentHashMap<String, DnsRecord> cache = new ConcurrentHashMap<>();
     private boolean cacheEnabled;
@@ -114,88 +108,69 @@ public class RobustDnsResolver {
     private RobustDnsResolver() {
     }
 
-    public Single<InetAddress> resolve(String name) {
+    public Single<DnsRecord> resolve(String name) {
         if(cache.containsKey(name)) {
             final DnsRecord record = cache.get(name);
             if(record.expireAt < System.currentTimeMillis()) {
-                System.out.println("cache miss");
-                cache.remove(name);
+                DnsRecord old = cache.remove(name);
                 if(updateOnExpire) {
                     resolve(name).subscribe();
-                    return Single.just(record.address);
+                    return Single.just(old);
                 } else {
                     return resolve(name);
                 }
             }
-            return Single.just(record.address);
+            return Single.just(record);
         }
         if(name == null || name.isEmpty()) {
             return Single.error(new UnknownHostException("invalid hostname : empty"));
         }
-        return Single.<InetAddress>create(emitter -> {
+        try {
             final Record question = Record.newRecord(Name.concatenate(Name.fromString(name), Name.root), Type.A, DClass.IN, TTL.MAX_VALUE);
             final Message query = Message.newQuery(question);
-
-            for (Resolver resolver : rsv) {
-                resolver.setTimeout(10);
-                resolver.sendAsync(query, new ResolverListener() {
-                    @Override
-                    public void receiveMessage(Object o, Message message) {
-                        Record[] records = message.getSectionArray(Section.ANSWER);
-
-                        if(records != null) {
-                            if(records.length == 0) {
-                                return;
-                            }
-                            try {
-                                final InetAddress resolved = Inet4Address.getByName(records[0].rdataToString());
-                                DnsRecord record = new DnsRecord(
-                                        resolved,
-                                        System.currentTimeMillis() + cacheTimeoutInMills
-                                );
-
-                                if(cacheEnabled) {
-                                    cache.put(name, record);
-                                }
-                                emitter.onSuccess(resolved);
-                            } catch (UnknownHostException e) {
-                                emitter.onError(e);
-                            }
-                        } else {
-                            emitter.onError(new UnknownHostException(String.format(Locale.ENGLISH, "%s not found", name)));
-                        }
-                    }
-
-                    @Override
-                    public void handleException(Object o, Exception e) {
-                        if(e instanceof PortUnreachableException) {
-                            disposable.add(Observable.just(name)
-                                    .subscribeOn(Schedulers.io())
-                                    .subscribe(unresolved -> {
-                                        try {
-                                            final InetAddress resolved = Address.getByName(name);
-                                            DnsRecord record = new DnsRecord(
-                                                    resolved,
-                                                    System.currentTimeMillis() + cacheTimeoutInMills
-                                            );
-                                            if (cacheEnabled) {
-                                                cache.put(name, record);
-                                            }
-                                            emitter.onSuccess(resolved);
-                                        } catch (Exception uke) {
-                                            // emitter.onError(uke);
-                                        }
-                                    }));
-                            return;
+            return Observable.fromArray(rsv)
+                    .map(resolver -> resolver.send(query))
+                    .map(message -> {
+                        final Record[] records = message.getSectionArray(Section.ANSWER);
+                        if (records == null || records.length == 0) {
+                            return DnsRecord.fail();
                         }
                         try {
-                            emitter.onSuccess(InetAddress.getByName(name));
-                        } catch (UnknownHostException ue) {
-                            emitter.onError(ue);
+                            final InetAddress resolved = Inet4Address.getByName(records[0].rdataToString());
+                            return DnsRecord.success(resolved, System.currentTimeMillis() + cacheTimeoutInMills);
+                        } catch (UnknownHostException e) {
+                            return DnsRecord.fail();
                         }
-                    }
-                });
-            }
-        }).timeout(10L, TimeUnit.SECONDS).subscribeOn(Schedulers.io());
+                    }).onErrorReturn(new Function<Throwable, DnsRecord>() {
+                        @Override
+                        public DnsRecord apply(Throwable throwable) throws Exception {
+                            if (throwable instanceof PortUnreachableException) {
+                                try {
+                                    final InetAddress resolved = Address.getByName(name);
+                                    return DnsRecord.success(resolved, System.currentTimeMillis() + cacheTimeoutInMills);
+                                } catch (Exception ignore) {
+                                    // failure case will be handled by failover (java default resolver)
+                                }
+                            }
+
+
+                            try {
+                                return DnsRecord.success(InetAddress.getByName(name), System.currentTimeMillis() + cacheTimeoutInMills);
+                            } catch (UnknownHostException ue) {
+                                return DnsRecord.fail();
+                            }
+                        }
+                    })
+                    .first(DnsRecord.fail())
+                    .doOnSuccess(record -> {
+                        if (record.isSuccessful() && cacheEnabled) {
+                            cache.put(name, record);
+                        }
+                    })
+                    .subscribeOn(Schedulers.io());
+
+        } catch (Exception e) {
+            return Single.just(DnsRecord.fail());
+        }
     }
 }
